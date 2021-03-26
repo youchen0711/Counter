198a3f1ebefc7484999e33aa1b176a1a6c951cc1
student <s1081692@gm.pu.edu.tw>
2021-03-26 15:02:41 +0800

    OnClickListener

--- app/src/main/java/tw/edu/pu/csim/s1081692/counter/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081692/counter/MainActivity.kt
@@ -6,13 +6,18 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     var counter:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+
+        button3.setOnClickListener(this)
 
     }
+
+
     fun Add(v: View){
         //if (v.id==R.id.button1){
         if (v==button1){
@@ -24,5 +29,26 @@ class MainActivity : AppCompatActivity() {
         txv.text = counter.toString()
     }
 
+
+
+    override fun onClick(view: View) {
+        if (view.id.equals(R.id.txv)){
+            counter++
+        }
+        else{
+            counter = 0
+        }
+        txv.text = counter.toString()
+    }
+
+    override fun onClick(view: View) {
+        if (view.id.equals(R.id.txv)){
+            counter++
+        }
+        else{
+            counter = 0
+        }
+        txv.text = counter.toString()
+    }
 
 }
